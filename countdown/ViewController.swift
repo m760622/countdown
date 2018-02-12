f7b97b056b65c2ab3c0a4d3f07486375c6705adf
Mohammed Abunada <mohnada76@gmail.com>
2018-02-12 12:49:29 +0100

    Initial Commit

---
+++ countdown/ViewController.swift
@@ -0,0 +1,98 @@
+//https://www.youtube.com/watch?time_continue=1&v=iNEjh6zDUsg
+//  ViewController.swift
+//  countdown
+//  a != nil ? a! : b
+//let rowHeight = contentHeight + (hasHeader ? 50 : 20)
+
+//  Created by Sebastian Hette on 26.09.2016.
+//  Copyright © 2016 MAGNUMIUM. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var seconds = 30
+    var timer = Timer()
+    var audioPlayer = AVAudioPlayer()
+    
+    @IBOutlet weak var minstLabel: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var sliderOutlet: UISlider!
+    @IBAction func slider(_ sender: UISlider)
+    {
+        seconds = Int(sender.value)
+        label.text = String(seconds) + " Seconds"
+    }
+    
+    @IBOutlet weak var startOutlet: UIButton!
+    @IBAction func start(_ sender: AnyObject)
+    {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.counter), userInfo: nil, repeats: true)
+        
+        sliderOutlet.isHidden = true
+        startOutlet.isHidden = true
+    }
+    
+    func counter()
+    {
+        seconds -= 1
+        label.text = String(seconds) + " Seconds"
+        
+        if (seconds == 0)
+        {
+            timer.invalidate()
+            audioPlayer.play()
+        }
+    }
+    
+    @IBOutlet weak var stopOutlet: UIButton!
+    @IBAction func stop(_ sender: AnyObject)
+    {
+        timer.invalidate()
+        seconds = 30
+        sliderOutlet.setValue(30, animated: true)
+        label.text = "30 Seconds"
+        
+        audioPlayer.stop()
+        
+        sliderOutlet.isHidden = false
+        startOutlet.isHidden = false
+    }
+    
+    
+
+    override func viewDidLoad()
+    {
+        
+         let minval = Int(sliderOutlet.minimumValue) , maxval = Int(sliderOutlet.maximumValue)
+        minstLabel.text = String(minval )
+        maxLabel.text = String(maxval )
+
+        super.viewDidLoad()
+        
+        do
+        {
+            let audioPath = Bundle.main.path(forResource: "1", ofType: ".mp3")
+            try audioPlayer = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+        }
+        catch
+        {
+            //ERROR
+        }
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
